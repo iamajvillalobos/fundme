68eb0fb195ecaaec6e0f236516209ba78b815041
Arman Jon Villalobos <me@armanjon.com>
2015-12-31 16:13:38 +0800

    Add test for listing of projects

---
+++ spec/features/list_projects_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "Viewing the list of projects" do
+  it "shows three projects" do
+    visit projects_url
+
+    expect(page).to have_text("3 Projects")
+    expect(page).to have_text("Project 1")
+    expect(page).to have_text("Project 2")
+    expect(page).to have_text("Project 3")
+  end
+end
