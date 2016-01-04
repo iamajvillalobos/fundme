f83cfff58e7709287a7472d027bf5ff4949b9e98
Arman Jon Villalobos <me@armanjon.com>
2016-01-05 01:40:55 +0800

    Fix formatting bugs, add seed and root route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.delete_all
+
+Project.create!([
+  {
+    name: "A Gay Ol' Time",
+    description: "An anthology of illustrated portraits and short biographical poems of American LGBTQIA and Two-Spirit people born in the 1800s.",
+    target_pledge_amount: 2950,
+    pledging_ends_on: 3.days.from_now,
+    website: "https://www.kickstarter.com/projects/1056477701/a-gay-ol-time?ref=discover_potd"
+  },
+
+  {
+    name: "Revelation Ale Works - Bringing craft beer to Hallock!",
+    description: "Be a Founder of our Northwest Minnesota craft brewery, Revelation Ale Works, launching in Hallock mid-2016!",
+    target_pledge_amount: 15000,
+    pledging_ends_on: 4.days.ago,
+    website: "https://www.kickstarter.com/projects/revales/revelation-ale-works-bringing-craft-beer-to-halloc?ref=recommended"
+  },
+
+  {
+    name: "Revols - Premium Quick Custom-Fit Wireless Earphones",
+    description: "Bluetooth earphones with tips that mold to the unique shape of your ears in 60 seconds. Unparalleled fit, comfort, sound and features.",
+    target_pledge_amount: 100000,
+    pledging_ends_on: 15.days.from_now,
+    website: "https://www.kickstarter.com/projects/revols/revols-premium-quick-custom-fit-wireless-earphones?ref=popular"
+  }
+])
--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -1,36 +1,36 @@
 describe "Viewing the list of projects" do
   it "shows three projects" do
     project1 = Project.create(name: "Help Save Theresa C Gusek's Life",
-               description: "I want to tell you how wonderful my friend, Theresa Corbin Gusek is and how we 
-               need your help to insure that Theresa can continue to be an amazing mother, wife, daughter, 
-               sister, friend and one of the finest first grade teachers we have. I want to say these things 
-               now so I don't have to say them at her funeral in the near future. We hope you will help us 
+               description: "I want to tell you how wonderful my friend, Theresa Corbin Gusek is and how we
+               need your help to insure that Theresa can continue to be an amazing mother, wife, daughter,
+               sister, friend and one of the finest first grade teachers we have. I want to say these things
+               now so I don't have to say them at her funeral in the near future. We hope you will help us
                save her life.",
                target_pledge_amount: 100000.00,
                pledging_ends_on: 10.day.from_now,
                website: "https://www.gofundme.com/theresag")
 
-    project2 = Project.create(name: "Police canine purchase", 
-                  description: "Hello, my name is William Westbrook and my canine is Maximus. I have been a 
-                  Police Officer for 17 years and recently suffered an injury that may end my career that 
-                  I love deeply. Maximus has been with me and my family for the last three years since he 
-                  was a puppy. Maximus was sent to me from Czechoslovakia and trained in Police work. 
-                  Maximus is trained in all forms of narcotic detection, tracking, apprehension and 
-                  handler protection. I am with Maximus 24/7 at work and at home. I was told by my department 
-                  that Maximus would be taken from me and reassigned to another officer when they retire me. 
-                  I was given the option to purchase my best friend for 15,000. With five kids and medical 
-                  bills I dont have the option or means to keep Maximus in the only home he has ever known. 
-                  I have never done anything like this and my pride makes telling my story hard but Maximus 
-                  is very important to me and my family. The department will start the transition within 
-                  the next few weeks. Maximus and my family would appreciate any help we can get to keep 
-                  him where he belongs. Any extra donations would be used to purchase bulletproof vests for 
+    project2 = Project.create(name: "Police canine purchase",
+                  description: "Hello, my name is William Westbrook and my canine is Maximus. I have been a
+                  Police Officer for 17 years and recently suffered an injury that may end my career that
+                  I love deeply. Maximus has been with me and my family for the last three years since he
+                  was a puppy. Maximus was sent to me from Czechoslovakia and trained in Police work.
+                  Maximus is trained in all forms of narcotic detection, tracking, apprehension and
+                  handler protection. I am with Maximus 24/7 at work and at home. I was told by my department
+                  that Maximus would be taken from me and reassigned to another officer when they retire me.
+                  I was given the option to purchase my best friend for 15,000. With five kids and medical
+                  bills I dont have the option or means to keep Maximus in the only home he has ever known.
+                  I have never done anything like this and my pride makes telling my story hard but Maximus
+                  is very important to me and my family. The department will start the transition within
+                  the next few weeks. Maximus and my family would appreciate any help we can get to keep
+                  him where he belongs. Any extra donations would be used to purchase bulletproof vests for
                   other Police canines to keep them all safe.",
                   target_pledge_amount: 15000.00,
                   pledging_ends_on: 7.day.from_now,
                   website: "https://www.gofundme.com/savek9maximus")
 
-    project3 = Project.create(name: "Facing Homelessness", 
-                  description: "When confronted with those suffering through homelessness, we 
+    project3 = Project.create(name: "Facing Homelessness",
+                  description: "When confronted with those suffering through homelessness, we
                   often find ourselves overwhelmed and left feeling at a loss for what to do.",
                   target_pledge_amount: 50000.00,
                   pledging_ends_on: 9.day.from_now,
@@ -45,7 +45,7 @@ describe "Viewing the list of projects" do
 
     expect(page).to have_text(project1.description[0..10])
     expect(page).to have_text("$100,000.00")
-    expect(page).to have_text(project1.pledging_ends_on)
+    expect(page).to have_text("9 days remaining")
     expect(page).to have_text(project1.website)
 
   end
