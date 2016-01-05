b295f509cab8f80ebe13324417b3211e9006b931
Arman Jon Villalobos <me@armanjon.com>
2016-01-05 22:52:59 +0800

    Add edit form and edit link

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "projects#index"
   get "projects" => "projects#index"
   get "project/:id" => "projects#show", as: 'project'
+  get "project/:id/edit" => "projects#edit", as: 'edit_project'
 end
